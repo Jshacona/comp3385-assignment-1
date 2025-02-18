ead7b7374808c97165d843f16c8a02f61d80fc7a
Jshacona Abbott <jshacona486@gmail.com>
2025-02-17 16:31:43 -0800

    Added the style for the page along with an image

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,4 +3,5 @@
 @section('content')
     <h2>Jshacona Abbott</h2>
     <p>Hi my name is Jshacona Abbott. I'm a student at the University of the West Indies Five Islands Campus studying Computer Science. I enjoy spending time with friends and family in my free time.</p>
+   <img src="{{ url('/images/web-development.jpg') }}" alt="Web Development Image" />
 @endsection
